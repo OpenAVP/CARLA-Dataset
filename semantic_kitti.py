b26695adb34ae5aa40dc61a9674dc707c549931c
Z.R. ZHAO <garry@azurflow.com>
2024-10-22 10:01:40 +0800

    Update DEV branch (#6)
    * FIX: change name of labels
    
    * FEAT: add semantic mapping.
    
    * FIX: change calib Tr format
    
    * FIX: change path name of sensors
    
    * FEAT: add map loading function
    
    * FIX: change points coordinate from left-handed to right-handed
    
    * FEAT: load map SUSTech_COE_ParkingLot
    
    * FEAT: set lidar attribute
    
    * FIX: correct name of directories and files
    
    * FEAT: wait for ego vehicle landing
    
    * FIX: change file name from label to labels
    
    * FEAT: make label mapping from carla to kitti
    
    * FIX: correct points coordinate from left-handed to right-handed
    
    * FIX: fix coordinate transformation
    
    * DOC: submodule diff
    
    * REFACTOR(#2): rename xxTargetPair to xxBind
    
    * DOCS: add docsting for xxBind
    
    * FEAT(#2): require specifying max_workers when using it in the function
    
    * DOCS(#2): add or update docstring
    
    * FEAT(#2): require specifying folder or path when using bind_xx() function
    
    * FEAT(#2): require specifying folder or path when using bind_xx() function
    
    * CHORE(#2): add temp folder with ignore settings
    
    * FIX(#2): argument missmatching after refactor
    
    * CHORE(#2): change default dump root
    
    * FEAT(#2): disable python default traceback
    
    * FEAT(#2): use context manager in creating sequence
    
    * FEAT(#2): use context manager in creating sequence
    
    * STYLE(#2): remove print and commented-out code
    
    * STYLE(#2): remove print and commented-out code
    
    * STYLE(#2): remove print and commented-out code
    
    * FEAT: upgrade carla1s version
    
    * FEAT: use fixed reload world cmd
    
    * FEAT: add entrypoint argparse for semantic kitti
    
    * FEAT: add separator to logs
    
    * Dev@refactor2443 b (#5)
    
    * FEAT: upgrade carla1s version
    * REFACTOR: fit carla1s new api about utils.logging
    
    ---------
    
    Co-authored-by: isleep <jessewang59@gmail.com>

--- semantic_kitti.py
+++ semantic_kitti.py
@@ -1,15 +1,25 @@
 import logging
+import time
+import argparse
 
-from packages.carla1s import CarlaContext, ManualExecutor
+from packages.carla1s import CarlaContext, ManualExecutor, PassiveExecutor
 from packages.carla1s.actors import Vehicle, RgbCamera, DepthCamera, SemanticLidar
 from packages.carla1s.tf import Transform
 
 from src.semantic_kitti import SemanticKittiDumper
 
 
-def main(*, fps: int = 20):   
+def main(*, 
+         fps: int = 20, 
+         map: str = 'Town01', 
+         output: str = './temp/', 
+         host: str = 'localhost', 
+         port: int = 2000,
+         log_level: int = logging.DEBUG):
 
-    with CarlaContext(host='10.0.0.110', log_level=logging.DEBUG) as cc, ManualExecutor(cc, fixed_delta_seconds=1/fps) as exe:        
+    with CarlaContext(host=host, port=port, log_level=log_level) as cc, ManualExecutor(cc, fixed_delta_seconds=1/fps) as exe:
+        cc.reload_world(map_name=map)
+        
         ego_vehicle: Vehicle = (cc.actor_factory
             .create(Vehicle, from_blueprint='vehicle.tesla.model3')
             .with_name("ego_vehicle")
@@ -18,7 +28,7 @@ def main(*, fps: int = 20):
         
         cam_0_tf = Transform(x=0.30, y=0.00, z=1.70)
         cam_1_tf = Transform(x=0.30, y=0.50, z=1.70)
-        semantic_lidar_tf = Transform(x=0.00, y=0.00, z=1.80)
+        semantic_lidar_tf = Transform(x=0.00, y=0.00, z=2.80)
 
         cam_0_rgb: RgbCamera = (cc.actor_factory
             .create(RgbCamera)
@@ -51,7 +61,13 @@ def main(*, fps: int = 20):
             .with_name("semantic_lidar")
             .with_transform(semantic_lidar_tf)
             .with_parent(ego_vehicle)
-            .with_attributes(rotation_frequency=fps)
+            .with_attributes(rotation_frequency=fps,
+                             points_per_second=1000000,
+                             channels=64,
+                             range=100,
+                             upper_fov=2,
+                             lower_fov=-24.8,
+                             )
             .build())
             
         cc.all_actors_spawn().all_sensors_listen()
@@ -59,25 +75,39 @@ def main(*, fps: int = 20):
         
         ego_vehicle.set_autopilot(True)
         exe.wait_ticks(1)
+        exe.wait_sim_seconds(1)
         
         # SETUP DUMPER
-        dumper = SemanticKittiDumper('/home/garry/Workspace/Temp/semantic_kitti')
-        dumper.bind_camera(cam_0_rgb, "image_0")
-        dumper.bind_camera(cam_1_rgb, "image_1")
-        dumper.bind_camera(cam_0_depth, "image_2")
-        dumper.bind_camera(cam_1_depth, "image_3")
-        dumper.bind_semantic_lidar(semantic_lidar, "velodyne", "label")
-        dumper.bind_timestamp(cam_0_rgb, "times.txt")
-        dumper.bind_pose(cam_0_rgb, "pose.txt")
-        dumper.bind_calib(semantic_lidar, "calib.txt")
+        dumper = SemanticKittiDumper(output)
+        dumper.bind_camera(cam_0_depth, data_folder="image_0")
+        dumper.bind_camera(cam_1_depth, data_folder="image_1")
+        dumper.bind_camera(cam_0_rgb, data_folder="image_2")
+        dumper.bind_camera(cam_1_rgb, data_folder="image_3")
+        dumper.bind_semantic_lidar(semantic_lidar, data_folder="velodyne", labels_folder="labels")
+        dumper.bind_timestamp(cam_0_rgb, file_path="times.txt")
+        dumper.bind_pose(cam_0_rgb, file_path="poses.txt")
+        dumper.bind_calib(tr_sensor=semantic_lidar, file_path="calib.txt")
 
-        dumper.create_sequence()
-        
-        
-        for i in range(3):
-            exe.wait_ticks(1)
-            dumper.create_frame().join()
-
+        # EXEC DUMP
+        with dumper.create_sequence():
+            for i in range(3):
+                dumper.logger.info(f'-> FRAME: {dumper.current_frame_name} '.ljust(80, '-'))
+                exe.wait_ticks(1)
+                dumper.create_frame().join()
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--fps', type=int, default=20, help='Recommended FPS of the simulation')
+    parser.add_argument('--map', type=str, default='Town01', help='Name of the map to load')
+    parser.add_argument('--output', type=str, default='./temp/', help='Path to save the dataset')
+    parser.add_argument('--host', type=str, default='localhost', help='Host of the Carla server')
+    parser.add_argument('--port', type=int, default=2000, help='Port of the Carla server')
+    parser.add_argument('--debug', action='store_true', help='Enable debug mode, setting log level to DEBUG')
+    args = parser.parse_args()
+    
+    log_level = logging.DEBUG if args.debug else logging.INFO
+    
+    try:
+        main(fps=args.fps, map=args.map, output=args.output, host=args.host, port=args.port, log_level=log_level)
+    except Exception:
+        print(f'Exception occurred, check the log for more details.')
